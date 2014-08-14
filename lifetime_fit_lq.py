93c8eaf96408a5cdf1feb1be0bec11f713ba288b
Avi Milder <amilder@umass.edu>
2014-08-14 15:00:50 -0400

    Substantial rework of global fitting

--- lifetime_fit_lq.py
+++ lifetime_fit_lq.py
@@ -27,7 +27,7 @@ print files
 index=0
 
 def readdata (files, data, tend):
-    for n in range (0,15):
+    for n in range(0,15):
         print n
         N=genfromtxt(files[n], names='lag, counts')
         d[n]=N['counts'][1:tend]
@@ -130,55 +130,76 @@ def fitdata(files,data,tend,n,p0,xl):
     return pfit
 
 def pre_modelglobal(x,g):
-        term={}
-        for n in range(0,15):
-            term[n]=(g[n]*1e3) * exp(-((x-tshift)/g[30]))+(g[n+15]*1e3) * exp(-((x-tshift)/g[31]))
-        #term0= (g[0]*1e3) * exp(-((x-tshift)/g[2]))
-	#term1= (g[1]*1e3) * exp(-((x-tshift)/g[3]))
-	#term2= (p[2]*1e4) * exp(-((x-tshift)/p[5]))
-	#term3= abs(par[3]) * exp(-(x-par[6])/(par[7]))
-	#fitfunc= term0+term1
-        term=term.values()
-	return term
-	
-	
-def errfunc2(g,x,y):
-	model=real(np.fft.ifftn(np.fft.fftn(pre_modelglobal(x,g)) * np.fft.fftn(irf[:]))) + g[32]
-        residual=(y-model) / sqrt(y)
-	#residual=asarray(residual).reshape(-1)
-        a=residual.shape
-        print g
-        return residual.flatten()
+    """
+    x is of shape (nPts,)
+    g is of shape (nCurves,)
+    returns (nCurves,nPts)
+    """
+    (tau1, tau2) = g[:2]
+    per_curve = g[2:]
+    amp1 = per_curve[len(files):2*len(files)]
+    amp2 = per_curve[2*len(files):3*len(files)]
 
-def globalfit(files,data,tend,g0,xl):
-    yl=data
-    pfit,cov,infodict,mesg,ier=leastsq(errfunc2,g0,args=(xl[:],yl),full_output=1)
+    curves  = (amp1[:,newaxis]*1e3) * exp(-((x[newaxis,:] - tshift) / tau1))
+    curves += (amp2[:,newaxis]*1e3) * exp(-((x[newaxis,:] - tshift) / tau2))
+    return curves
+	
+def model_global(x,g,irf):
+    """
+    x is of shape (nPts,)
+    y is of shape (nCurves,nPts)
+    g is of shape (nCurves,)
+    returns (nCurves,nPts)
+    """
+    per_curve = x[2:]
+    offsets = per_curve[0:len(files)]
+    curves = pre_modelglobal(x,g)
+    models = []
+    for (offset, m) in zip(offsets, curves):
+	model = real(np.fft.ifft(np.fft.fft(m) * np.fft.fft(irf)))
+        model += offset
+        models.append(model)
+    return np.array(models)
+    
+def errfunc2(g,x,y,irf):
+    """
+    x is of shape (nPts,)
+    y is of shape (nCurves,nPts)
+    g is of shape (nCurves,)
+    returns (nCurves*nPts)
+    """
+    print g
+    return ((y - model_global(x,g,irf)) / sqrt(y)).flatten()
+
+def globalfit(yl,tend,g0,xl,irf):
+    pfit,cov,infodict,mesg,ier=leastsq(errfunc2,g0,args=(xl[:],yl,irf),full_output=1)
     chisq=(infodict['fvec']**2).sum()
     dof=len(xl[start:])-len(pfit) #
     
-    pre_mod_final=pre_model(xl[:],pfit[:4])
-    final_model=real(ifft(fft(pre_mod_final) * fft(irf[:]))) + pfit[4]
-    
-    pname=['A0','A1','tau0','tau1','offset']
-    print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-    print "converged with chi squared", chisq
-    print "degrees of freedom, dof", dof
-    print "Reduced chisq (variance of rsiduals)", chisq/dof
-    print
-    print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    #pname=['A0','A1','tau0','tau1','offset']
+    #print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    #print "converged with chi squared", chisq
+    #print "degrees of freedom, dof", dof
+    #print "Reduced chisq (variance of rsiduals)", chisq/dof
+    #print
+    #print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    ##for i,pmin in enumerate(pfit):
+    ##	print "%2i % -10s %12f+/- %10f "%(i,pname[i],pmin,sqrt(cov[i,i])*sqrt(chisq/dof))
+    #	#,sqrt(cov[i,i])*sqrt(chisq/dof))+/- %10f
     #for i,pmin in enumerate(pfit):
-    #	print "%2i % -10s %12f+/- %10f "%(i,pname[i],pmin,sqrt(cov[i,i])*sqrt(chisq/dof))
-    	#,sqrt(cov[i,i])*sqrt(chisq/dof))+/- %10f
-    for i,pmin in enumerate(pfit):
-    	print  i,pname[i],pmin
-    
+    #	print  i,pname[i],pmin
+    return pfit
+
+def plot_global(yl, p, xl, irf):
+    final_model = model_global(xl[:], p, irf)
+
     fig=figure()
     suptitle("global")
     ax1=subplot2grid((4,1),(0,0),rowspan=3)
     #ax1.plot(xl[:],irf[:])
     for n in range(0,15):
         ax1.plot(xl[:],yl[n],color='k',linewidth=3)#
-    ax1.plot(xl[:],final_model[:],color='r',linewidth=2)#
+        ax1.plot(xl[:],final_model[n],color='r',linewidth=2)#
     #ax1.set_xlim(2.5,10)
     ax1.set_ylabel('intensity (arb. units)',fontsize=18)
     ax1.yaxis.set_tick_params(labelsize=15)
@@ -186,7 +207,7 @@ def globalfit(files,data,tend,g0,xl):
     
     ax2=subplot2grid((4,1),(3,0),rowspan=1)
     for n in range(0,15):
-        ax2.plot(xl[:],(final_model[:]-yl[n]),'.',color='k')#
+        ax2.plot(xl[:],(final_model[n]-yl[n]),'.',color='k')#
     ax2.axhline(y=0)
     ax2.set_ylim(-300,300)
     #ax2.set_yticks([-200,-100,0,100,200])
@@ -197,7 +218,6 @@ def globalfit(files,data,tend,g0,xl):
     ax2.set_ylabel('residuals',fontsize=18)
     
     fig.show()
-    return pfit
 
 pfit={}
 tau1s=zeros(15)
@@ -225,9 +245,16 @@ plot(xs,a2, 'bv', label="A 2")
 legend()
 #fig.show()
 
-g=ones(33)
-g[30]=3.8
-g[31]=.2
-g[32]=30
-gfit=globalfit(files,d,tend,g,xl)
+# g is offsets+amp1+amp2+[tau1,tau2]
+g = ones(3*len(files) + 2)
+for i,file in enumerate(d):
+    g[i] = d.min()
+g[0]=3.8  # tau1
+g[1]=0.2  # tau2
+plot_global(d, g, xl, irf)
+dec = 4 # decimation factor
+gfit=globalfit(d[:,::dec],tend,g,xl[::dec],irf[::dec])
+plot_global(d, gfit, xl, irf)
+gfit=globalfit(d,tend,gfit,xl,irf)
+plot_global(d, gfit, xl, irf)
 print gfit
